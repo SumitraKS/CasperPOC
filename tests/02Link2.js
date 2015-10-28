773a1a081467e37e5a668bf5ad482646a6d9b7a8
SumitraKS <sumitra.k.s@gmail.com>
2015-10-28 17:27:33 +0530

    Update 02Link2.js

--- tests/02Link2.js
+++ tests/02Link2.js
@@ -2,7 +2,7 @@ var url = 'http://www.helpscout.net/blog/functional-testing-casperjs/';
 casper.test.begin('Page 2', function suite(test) {
 	casper.start(url);
 	casper.then(function() {
-	this.test.assertHttpStatus(200, 'Page 1');
+	this.test.assertHttpStatus(200, 'Page 2');
 });
 casper.then(function() {
 	this.test.assertExists(".lead", "Link1 1");
